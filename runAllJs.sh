bea2d82682bf8d186d6a9c22d548ce9e75a17fce
Patrick Flor <git.push.origin@gmail.com>
2020-12-05 00:17:26 -0600

    🧼 Clean up "test suite"

--- runAllJs.sh
+++ runAllJs.sh
@@ -1,28 +1,25 @@
 #! /usr/bin/env bash
 
-# scope shopt in case sourced
-(
-  shopt -s globstar 2> /dev/null
-  declare -a broken
-
-  while read -r file; do
-    echo $file
-    pushd $(dirname "$file") > /dev/null
-    node $(basename "$file")
-    if [ $? -ne 0 ]; then
-      broken+=( "$file" )
-    fi
-    popd > /dev/null
-    echo
-  done < <(ls | grep -E '\d+' | xargs -I % find  % -name '*.js')
-
-  if [ ${#broken[@]} -eq 0 ]; then
-    echo "EVERYTHING IS AWESOME"
-  else
-    echo "BROKEN SCRIPTS:"
-    for broken_script in "${broken[@]}"
-    do
-      echo "$broken_script"
-    done
+declare -a broken
+while read -r file; do
+  echo $file
+  pushd $(dirname "$file") > /dev/null
+  node $(basename "$file")
+  if [ $? -ne 0 ]; then
+    broken+=( "$file" )
   fi
-)
+  popd > /dev/null
+  echo
+done < <(ls | grep -E '\d+' | xargs -I % find  % -name '*.js')
+# ^ `ls` to `find` pipeline maintains per-day folder order
+
+if [ ${#broken[@]} -eq 0 ]; then
+  echo "EVERYTHING IS AWESOME"
+else
+  echo "BROKEN SCRIPTS:"
+  for broken_script in "${broken[@]}"
+  do
+    echo "$broken_script"
+  done
+fi
+unset broken # in case file is sourced
