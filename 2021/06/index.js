54803bcc97e48a6ffba6b39ebfb12caa0dd687f6
Patrick Flor <git.push.origin@gmail.com>
2021-12-06 19:14:47 -0600

    Add problem 6, v2

--- 2021/06/index.js
+++ 2021/06/index.js
@@ -4,17 +4,19 @@ const input = require('fs').readFileSync(
 ).trim().split(',')
 
 // Part 1
+const fishCount = (arr) => arr.reduce((acc, curr) => acc + curr, 0)
 const initial = input.reduce(
   (acc, n) => { acc[n] += 1; return acc },
   Array(9).fill(0)
 )
-const spawn = ([f0, f1, f2, f3, f4, f5, f6, f7, f8], days) => {
-  if (days < 1) {
-    return f0 + f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8
-  }
-  return spawn([f1, f2, f3, f4, f5, f6, f7 + f0, f8, f0], days - 1)
+for (let i = 0; i < 80; i++) {
+  initial[(i + 7) % 9] += initial[i % 9]
 }
-console.log(spawn(initial, 80))
+console.log(fishCount(initial))
 
-// Part 2
-console.log(spawn(initial, 256))
+// Part 2; retain results of previous part for silly small efficieny gain
+// Note the re-starting iteration, 81, mod 9, is 0.
+for (let i = 0; i < 176; i++) {
+  initial[(i + 7) % 9] += initial[i % 9]
+}
+console.log(fishCount(initial))
