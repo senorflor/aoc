640d571980e1d982403a17639302e323270f4909
Patrick Flor <git.push.origin@gmail.com>
2021-12-08 22:19:04 -0600

    🍽️  Make template evaluable in node REPL

--- 2021/template.js
+++ 2021/template.js
@@ -1,5 +1,21 @@
-const input = require('fs').readFileSync(
-  __dirname + '/input.txt',
-  'utf8'
-).trim().split('\n')
+// __dirname does not exist in REPL env.
+let dirname
+try { dirname = __dirname } catch { dirname = '.' }
 
+// Get input
+const input = require('fs').readFileSync(
+  dirname + '/input.txt',
+  'utf8'
+).trim().split('\n').map(
+  // Parse here
+  // Don't forget to parseInt() / Number() as necessary.
+  l => l
+)
+
+// Part 1
+let part1 = input.length
+console.log(part1)
+
+// Part 2
+let part2 = input.length
+console.log(part2)
