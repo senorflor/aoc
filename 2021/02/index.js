4a3148d2809acf6e09dbe1f6d02fc93085401d1d
Patrick Flor <git.push.origin@gmail.com>
2021-12-08 22:01:10 -0600

    🥈  Add day 2 soln. that was sitting around

---
+++ 2021/02/index.js
@@ -0,0 +1,49 @@
+const input = require('fs').readFileSync(
+  __dirname + '/input.txt',
+  'utf8'
+).trim().split('\n').map(
+  // Parse here
+  l => l.split(' ')
+)
+
+// Part 1
+let h = 0
+let d = 0
+input.forEach(([dir, n]) => {
+  n = parseInt(n)
+  switch (dir) {
+    case 'forward':
+      h += n
+      break;
+    case 'up':
+      d -= n
+      break;
+    case 'down':
+      d += n
+      break;
+  }
+})
+const part1 = d * h
+console.log(part1)
+
+// Part 2
+h = 0
+d = 0
+let aim = 0
+input.forEach(([dir, n]) => {
+  n = parseInt(n)
+  switch (dir) {
+    case 'forward':
+      h += n
+      d += aim * n
+      break;
+    case 'up':
+      aim -= n
+      break;
+    case 'down':
+      aim += n
+      break;
+  }
+})
+const part2 = h * d
+console.log(part2)
