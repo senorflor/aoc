e6f4bde287107c7dee0537d32b1055a71ba65343
Patrick Flor <git.push.origin@gmail.com>
2021-12-11 14:34:49 -0600

    ♻️ Rationalize part 1 naming + constness

--- 2021/03/index.js
+++ 2021/03/index.js
@@ -2,18 +2,18 @@ const input = require('fs').readFileSync(
   __dirname + '/input.txt',
   'utf8'
 ).trim().split('\n').map(
-  l => l.trim().split('').map(s => parseInt(s))
+  l => l.trim().split('').map(Number)
 )
 // Part 1
-let acc = Array(12).fill(0)
-input.forEach(row => {
-  for (let i = 0; i < row.length; i++) {
-    acc[i] += row[i]
+const placeTotals = Array(input[0].length).fill(0)
+input.forEach(reading => {
+  for (let i = 0; i < reading.length; i++) {
+    placeTotals[i] += reading[i]
   }
 })
 const threshold = input.length / 2
 const gamma = parseInt(
-  acc.map(n => n > threshold ? '1' : '0').join(''),
+  placeTotals.map(n => n > threshold ? '1' : '0').join(''),
   2
 )
 const TOTAL = 2 ** 12 - 1
@@ -26,19 +26,19 @@ let o2 = [...input]
 let co2 = [...input]
 for (let i = 0; i < 12 && o2.length > 1; i++) {
   let count = 0
-  o2.forEach(row => {
-    count += row[i] ? 1 : -1
+  o2.forEach(reading => {
+    count += reading[i] ? 1 : -1
   })
   let bit = (count < 0) ? 0 : 1
-  o2 = o2.filter(row => row[i] === bit)
+  o2 = o2.filter(reading => reading[i] === bit)
 }
 for (let i = 0; i < 12 && co2.length > 1; i++) {
   let count = 0
-  co2.forEach(row => {
-    count += row[i] ? 1 : -1
+  co2.forEach(reading => {
+    count += reading[i] ? 1 : -1
   })
   let bit = (count < 0) ? 1 : 0
-  co2 = co2.filter(row => row[i] === bit)
+  co2 = co2.filter(reading => reading[i] === bit)
 }
 const o2rating = parseInt(o2[0].join(''), 2)
 const co2rating = parseInt(co2[0].join(''), 2)
