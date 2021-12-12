cc01aa915dfc8c602060b3d7d908f1839b5a2145
Patrick Flor <git.push.origin@gmail.com>
2021-12-11 20:28:56 -0600

    7️⃣  Add day 07 solution

--- 2021/07/index.js
+++ 2021/07/index.js
@@ -1,32 +1,28 @@
 const input = require('fs').readFileSync(
   __dirname + '/input.txt',
   'utf8'
-).trim().split(',').map(
-  // Parse here
-  // Don't forget to parseInt() / Number() as necessary.
-  Number
-)
+).trim().split(',').map(Number)
 
 // Part 1
-// TODO: Piece together part 1 again since I just hacked in place over part 1 to do part 2.
 const min = Math.min(...input)
 const max = Math.max(...input)
-let minFuel = +Infinity
-let pos
+let minFuel1 = +Infinity
+let minFuel2 = +Infinity
 for (let i = min; i <= max; i++) {
-  const fuel = input.map(
+  const [fuel1, fuel2] = input.map(
     n => {
       const d = Math.abs(i - n)
-      return (d ** 2 + d) / 2
+      return [d, (d ** 2 + d) / 2]
     }
-  ).reduce((acc, n) => acc + n)
-  if (fuel < minFuel) {
-    minFuel = fuel
-    pos = i
+  ).reduce(([acc1, acc2], [f1, f2]) => [acc1 + f1, acc2 + f2])
+  if (fuel1 < minFuel1) {
+    minFuel1 = fuel1
+  }
+  if (fuel2 < minFuel2) {
+    minFuel2 = fuel2
   }
 }
-console.log(minFuel)
+console.log(minFuel1)
 
 // Part 2
-let part2 = input.length
-console.log(part2)
+console.log(minFuel2)
