0e7b5eb442bf90a310453f0c93c6ebd88efae3da
Patrick Flor <git.push.origin@gmail.com>
2021-12-11 20:18:55 -0600

    ♻️ Tweak var naming in day 06

--- 2021/06/index.js
+++ 2021/06/index.js
@@ -5,18 +5,18 @@ const input = require('fs').readFileSync(
 
 // Part 1
 const fishCount = (arr) => arr.reduce((acc, curr) => acc + curr, 0)
-const initial = input.reduce(
+const groupedFish = input.reduce(
   (acc, n) => { acc[n] += 1; return acc },
   Array(9).fill(0)
 )
 for (let i = 0; i < 80; i++) {
-  initial[(i + 7) % 9] += initial[i % 9]
+  groupedFish[(i + 7) % 9] += groupedFish[i % 9]
 }
-console.log(fishCount(initial))
+console.log(fishCount(groupedFish))
 
 // Part 2; retain results of previous part for silly small efficieny gain
 // Note the re-starting iteration, 81, mod 9, is 0.
 for (let i = 80; i < 256; i++) {
-  initial[(i + 7) % 9] += initial[i % 9]
+  groupedFish[(i + 7) % 9] += groupedFish[i % 9]
 }
-console.log(fishCount(initial))
+console.log(fishCount(groupedFish))
