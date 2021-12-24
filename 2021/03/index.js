287aa54aedbb4e781235afb93de974740163aca7
Patrick Flor <git.push.origin@gmail.com>
2021-12-24 13:32:43 -0600

    🚉 Eliminate top-level state, day 03

--- 2021/03/index.js
+++ 2021/03/index.js
@@ -1,55 +1,63 @@
-const input = require('fs').readFileSync(
-  __dirname + '/input.txt',
-  'utf8'
-).trim().split('\n').map(
-  l => l.trim().split('').map(Number)
-)
+const input = require('./input.js')
+  .split('\n')
+  .map(l => l
+    .trim()
+    .split('')
+    .map(Number)
+  )
+// Helpers
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2)
 
 // Part 1
-const READING_WIDTH = input[0].length
-const TOTAL = 2 ** READING_WIDTH - 1 // gamma + epsilon always complement to this total
-const placeTotals = Array(READING_WIDTH).fill(0)
-input.forEach(reading => {
-  for (let i = 0; i < reading.length; i++) {
-    placeTotals[i] += reading[i]
-  }
-})
-const threshold = input.length / 2
-const gamma = parseInt(
-  placeTotals.map(n => n > threshold ? '1' : '0').join(''),
-  2
-)
-const epsilon = TOTAL - gamma
-const powerConsumption = gamma * epsilon
-console.log(powerConsumption)
+const powerConsumption = (readings) => {
+  const threshold = readings.length / 2
+  const readingLength = readings[0].length
+  const bitwisePowerConsumption = readings
+    .reduce(
+      (totals, r) => totals.map((total, place) => total += r.at(place)),
+      Array(readingLength).fill(0)
+    )
+    .map(
+      count => count > threshold ? '1' : '0'
+    )
+  const gamma = binaryArrayToNumber(bitwisePowerConsumption)
+  const epsilon = 2 ** readingLength - 1 - gamma
+  return gamma * epsilon
+}
+
+console.log(powerConsumption(input))
 
 // Part 2
+const lifeSupportRating = (readings) => {
+  const readingLength = readings[0].length
 
-// Find correct reading for o2 rating:
-let o2 = [...input]
-for (let i = 0; i < READING_WIDTH && o2.length > 1; i++) {
-  let count = 0
-  o2.forEach(reading => {
-    count += reading[i] ? 1 : -1
-  })
-  const bit = (count < 0) ? 0 : 1
-  o2 = o2.filter(reading => reading[i] === bit)
+  // Find correct reading for o2 rating:
+  let o2 = [...readings]
+  for (let i = 0; i < readingLength && o2.length > 1; i++) {
+    let count = 0
+    o2.forEach(reading => {
+      count += reading[i] ? 1 : -1
+    })
+    const bit = (count < 0) ? 0 : 1
+    o2 = o2.filter(reading => reading[i] === bit)
+  }
+
+  // Find correct reading for co2 rating:
+  let co2 = [...readings]
+  for (let i = 0; i < readingLength && co2.length > 1; i++) {
+    let count = 0
+    co2.forEach(reading => {
+      count += reading[i] ? 1 : -1
+    })
+    const bit = (count < 0) ? 1 : 0
+    co2 = co2.filter(reading => reading[i] === bit)
+  }
+
+  // Calculate final rating
+  const o2rating = binaryArrayToNumber(o2[0])
+  const co2rating = binaryArrayToNumber(co2[0])
+  const lifeSupportRating = o2rating * co2rating
+  return lifeSupportRating
 }
 
-// Find correct reading for co2 rating:
-let co2 = [...input]
-for (let i = 0; i < READING_WIDTH && co2.length > 1; i++) {
-  let count = 0
-  co2.forEach(reading => {
-    count += reading[i] ? 1 : -1
-  })
-  const bit = (count < 0) ? 1 : 0
-  co2 = co2.filter(reading => reading[i] === bit)
-}
-
-// Calculate final rating
-const binaryArrayToNumber = arr => parseInt(arr.join(''), 2)
-const o2rating = binaryArrayToNumber(o2[0])
-const co2rating = binaryArrayToNumber(co2[0])
-const lifeSupportRating = o2rating * co2rating
-console.log(lifeSupportRating)
+console.log(lifeSupportRating(input))
