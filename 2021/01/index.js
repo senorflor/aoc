f30d017fc9ee43be9b1213928808b04ad0c7f297
Patrick Flor <git.push.origin@gmail.com>
2021-12-24 11:22:35 -0600

    🚉  Eliminate top-level state, day 1

--- 2021/01/index.js
+++ 2021/01/index.js
@@ -3,20 +3,18 @@ const input = require('fs').readFileSync(
   'utf8'
 ).trim().split('\n').map(Number)
 
-// Part 1
-let ans1 = 0
-for (let i = 1; i < input.length; i++) {
-  const start = input[i - 1]
-  const end = input[i]
-  ans1 += end > start ? 1 : 0
+const countDescents = (depths, windowSize) => {
+  let count = 0
+  for (let x = windowSize; x < depths.length; x++) {
+    const start = depths[x - windowSize]
+    const end = depths[x]
+    count += end > start ? 1 : 0
+  }
+  return count
 }
-console.log(ans1)
+
+// Part 1
+console.log(countDescents(input, 1))
 
 // Part 2
-let ans2 = 0
-for (let i = 3; i < input.length; i++) {
-  const start = input[i - 3]
-  const end = input[i]
-  ans2 += end > start ? 1 : 0
-}
-console.log(ans2)
+console.log(countDescents(input, 3))
