8c88011bfd96d66f97dadab829427f4e358ed552
Patrick Flor <git.push.origin@gmail.com>
2021-12-24 15:14:48 -0600

    🎛️  Simplify control flow

--- 2021/03/index.js
+++ 2021/03/index.js
@@ -31,33 +31,32 @@ console.log(powerConsumption(input))
 const lifeSupportRating = (readings) => {
   const readingLength = readings[0].length
 
-  // Find correct reading for o2 rating:
+  // Find correct readings for o2 and co2
   let o2 = [...readings]
-  for (let i = 0; i < readingLength && o2.length > 1; i++) {
-    let count = 0
-    o2.forEach(reading => {
-      count += reading[i] ? 1 : -1
-    })
-    const bit = (count < 0) ? 0 : 1
-    o2 = o2.filter(reading => reading[i] === bit)
-  }
-
-  // Find correct reading for co2 rating:
   let co2 = [...readings]
-  for (let i = 0; i < readingLength && co2.length > 1; i++) {
-    let count = 0
-    co2.forEach(reading => {
-      count += reading[i] ? 1 : -1
-    })
-    const bit = (count < 0) ? 1 : 0
-    co2 = co2.filter(reading => reading[i] === bit)
-  }
+  for (let i = 0; i < readingLength; i++) {
+    if (o2.length > 1) {
+      let count = 0
+      o2.forEach(reading => {
+        count += reading[i] ? 1 : -1
+      })
+      const bit = (count < 0) ? 0 : 1
+      o2 = o2.filter(reading => reading[i] === bit)
+    }
+    if (co2.length > 1) {
+      let count = 0
+      co2.forEach(reading => {
+        count += reading[i] ? 1 : -1
+      })
+      const bit = (count < 0) ? 1 : 0
+      co2 = co2.filter(reading => reading[i] === bit)
+    }
+  } 
 
   // Calculate final rating
   const o2rating = binaryArrayToNumber(o2[0])
   const co2rating = binaryArrayToNumber(co2[0])
-  const lifeSupportRating = o2rating * co2rating
-  return lifeSupportRating
+  return o2rating * co2rating
 }
 
 console.log(lifeSupportRating(input))
